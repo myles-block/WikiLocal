791472f8a220f9a8f7f44d1de16328a9bcd118f1
Gabriel Bremo <gabremob@techexchange.in>
2023-03-01 23:11:05 +0000

    I updated the constructor to initialize a storage client and the contents bucket

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,14 +1,24 @@
 # TODO(Project 1): Implement Backend according to the requirements.
+from google.cloud import storage
+
 class Backend:
 
-    def __init__(self):
-        pass
+    def __init__(self, storage_client = storage.Client(), info_bucket_name = 'wiki_info'):
+        self.storage_client = storage_client
+        self.info_bucket = self.storage_client.bucket(info_bucket_name)
         
     def get_wiki_page(self, name):
         pass
 
     def get_all_page_names(self):
-        pass
+        page_names = []
+
+        pages = self.storage_client.list_blobs(self.info_bucket)
+
+        for page in pages:
+            page_names.append(page.name)
+
+        return page_names
 
     def upload(self):
         pass
