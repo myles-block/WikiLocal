6874fa5711acefea4ac50411608f8940da45556f
Manish Niure <manishniure@techexchange.in>
2023-03-08 04:48:05 +0000

    made some modification on get_image

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -6,43 +6,32 @@ import base64
 
 class Backend:
 
-    def __init__(self,bucket_name='wiki_info'):
+    def __init__(self,info_bucket_name='wiki_info'):
         '''
         client :  Instantiates a client
         '''
         self.client = storage.Client()
-        self.bucket = self.client.bucket(bucket_name)
+        self.info_bucket= self.client.bucket(info_bucket_name)
         
     def get_wiki_page(self, name): # 1 
+        ''' Gets an uploaded page from the content bucket '''
 
         ''' Gets an uploaded page from the content bucket '''
-        updated_name = name + '.txt'
-        blob = self.bucket.blob(updated_name)
+        blob = self.info_bucket.blob(name)
         name_data = blob.download_as_bytes()
         if not name_data.strip():
             return None 
         return name_data.decode('utf-8')
 
 
+
     def get_all_page_names(self):
-        page_names = []
-        blob_lists = self.client.list_blobs(self.bucket)
-        print(blob_lists)
-        for content in blob_lists:
-            if content.name.endswith('.txt'):
-                page_name = content.name.split('.')[0]
-                page_names.append(page_name)
-        return page_names
+        pass 
 
     def upload(self,file,filename):
+        pass
 
-        ''' Adds data to the content bucket 
-         file : path of the file 
-         filename : name of the file user selected
-         '''
-        blob = self.bucket.blob(filename)
-        blob.upload_from_file(file)
-
+        
     def sign_up(self):
         pass
 
@@ -50,19 +39,18 @@ class Backend:
         pass
 
     def get_image(self,image_name): # 2
-        ''' Gets an image from the content bucket. '''
-        blob = self.bucket.blob(image_name)
-        image_data=blob.download_as_bytes()
-        if image_data:
-            base64_image=base64.b64encode(image_data).decode('utf-8')
-            return base64_image
-        else:
-            return 'Error'
-        
-# custom=Backend('wiki_info')
-# print(custom.get_all_page_names())
-# print(custom.get_wiki_page('GeorgeTown Waterfront Park'))
 
+        ''' Gets an image from the content bucket. '''
+
+        try: 
+            blob = self.info_bucket.blob(image_name)
+            image_data=blob.download_as_bytes()
+            if image_data:
+                base64_image=base64.b64encode(image_data).decode('utf-8')
+                return base64_image
+        except FileNotFoundError : #handling the not existing file
+            raise ValueError('Image Name doesnot exist in the bucket')
+        
 
         
     
