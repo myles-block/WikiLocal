0ce4671e1eabdb81095de6413ad7658d718ebda9
mylesblock <mylesblock@techexchange.in>
2023-04-18 05:13:26 +0000

    Fixed get image test cases

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -144,13 +144,13 @@ def test_upload(backend, fake_blob):
             content_type='application/json')
 
 
-def test_get_image(backend, fake_blob):
+def test_get_image_upload(backend, fake_blob):
 
     #mocking the download_as_bytes
     fake_blob.download_as_bytes.return_value = b'Fake_Image_Data'
 
     # getting the dummy data
-    result = backend.get_image('fake_image.jpeg')
+    result = backend.get_image('fake_image.jpeg', "wiki_info")
     expected = base64.b64encode(b'Fake_Image_Data').decode('utf-8')
     assert result == expected
 
@@ -159,14 +159,44 @@ def test_get_image(backend, fake_blob):
     fake_blob.download_as_bytes.assert_called_once()
 
 
-def test_get_image_failure(backend, fake_blob):
+def test_get_image_account(backend, fake_blob):
+    #mocking the download_as_bytes
+    fake_blob.download_as_bytes.return_value = b'Fake_Image_Data'
+
+    # getting the dummy data
+    result = backend.get_image('fake_image.jpeg', "wiki_login")
+    expected = base64.b64encode(b'Fake_Image_Data').decode('utf-8')
+    assert result == expected
+
+    # checking the calls to the backend and blob
+    backend.user_bucket.blob.assert_called_once_with('fake_image.jpeg')
+    fake_blob.download_as_bytes.assert_called_once()
+
+
+def test_get_image_failure_upload(backend, fake_blob):
 
     #mocking the download_as_bytes and using side_effects for exception instance
     fake_blob.download_as_bytes.side_effect = FileNotFoundError()
 
     #checking if value error raises or not in the backend
     with pytest.raises(ValueError) as v:
-        backend.get_image('fake_not_existing_image.jpeg')
+        backend.get_image('fake_not_existing_image.jpeg', "wiki_login")
+    assert str(v.value) == 'Image Name does not exist in the bucket'
+
+    #checking the calls to the backend and blob
+    backend.user_bucket.blob.assert_called_once_with(
+        'fake_not_existing_image.jpeg')
+    fake_blob.download_as_bytes.assert_called_once()
+
+
+def test_get_image_failure_account(backend, fake_blob):
+
+    #mocking the download_as_bytes and using side_effects for exception instance
+    fake_blob.download_as_bytes.side_effect = FileNotFoundError()
+
+    #checking if value error raises or not in the backend
+    with pytest.raises(ValueError) as v:
+        backend.get_image('fake_not_existing_image.jpeg', "wiki_info")
     assert str(v.value) == 'Image Name does not exist in the bucket'
 
     #checking the calls to the backend and blob
